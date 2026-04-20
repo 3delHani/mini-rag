9257e08338769cdb31f039bd56f00ac8a323df82
Adel <adelhani116@gmail.com>
2026-04-20 02:28:36 +0200

    make assets in database and its schemes and update routes.data.py

--- src/models/enums/DataBaseEnum.py
+++ src/models/enums/DataBaseEnum.py
@@ -5,3 +5,4 @@ class DataBaseEnum(Enum):
     COLLECTION_PROJECT_NAME = "projects"
     COLLECTION_CHUNK_NAME = "chunks"
     COLLECTION_METADATA_NAME = "metadata"
+    COLLECTION_ASSET_NAME = "assets"
