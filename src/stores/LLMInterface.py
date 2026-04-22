cbef41b49ec95c21f2272ff4f7a54fd02df23143
Adel <adelhani116@gmail.com>
2026-04-22 21:12:13 +0200

    create interface for openai model and irts enum

---
+++ src/stores/LLMInterface.py
@@ -0,0 +1,25 @@
+from abc import ABC, abstractmethod
+from typing import Any
+
+class LLMInterface(ABC):
+    
+    @abstractmethod
+    def set_generation_model(self, model_id: str):
+        pass
+    
+    @abstractmethod
+    def set_embedding_model(self, model_id: str, embedding_size: int):
+        pass
+    
+    @abstractmethod
+    def generate_text(self, prompt: str, chat_histtory: list[Any] = [], max_output_tokens: int | None = None,
+                            temprature: float | None = None) -> Any:
+        pass
+    
+    @abstractmethod
+    def embed_text(self, text: str, document_type: str | None = None):
+        pass
+    
+    @abstractmethod
+    def construct_prompt(self, prompt: str, role: str):
+        pass
